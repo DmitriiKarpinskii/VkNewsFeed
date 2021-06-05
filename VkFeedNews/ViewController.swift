9f27f1eafee5fa77627ad0bf02fabaeeab9ae1c0
Dmitry Karpinsky <karpinsky.d.s@gmail.com>
2021-06-05 14:35:52 +0300

    Initial Commit

---
+++ VkFeedNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkFeedNews
+//
+//  Created by Dmitry Karpinsky on 05.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
